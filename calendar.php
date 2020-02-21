0561e93249fe787ca31538314fe429a2f10eb007
Chicodemilo <mileschick@gmail.com>
2020-02-21 14:12:38 -0600

    api variable change

--- calendar.php
+++ calendar.php
@@ -63,7 +63,7 @@ $service = new Google_Service_Calendar($client);
 date_default_timezone_set('America/Chicago');
 
 $optParams = array(
-    'maxResults' => 16,
+    'maxResults' => 17,
     'orderBy' => 'startTime',
     'singleEvents' => true,
     'timeMin' => date(
