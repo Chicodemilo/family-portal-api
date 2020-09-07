522f9276844b82325d1bd70e5050991e1b1f1a51
Chicodemilo <mileschick@gmail.com>
2020-09-07 10:40:16 -0500

    changing select by id

--- getStatus.php
+++ getStatus.php
@@ -6,10 +6,11 @@ $stmt = $conn->query("SELECT u.id, u.name FROM users as u");
 $users = $stmt->fetchAll();
 
 foreach ($users as $key => $user) {
-    $stmt2 = $conn->prepare("SELECT status, created_at FROM status WHERE user_id=? ORDER BY created_at DESC LIMIT 1");
+    $stmt2 = $conn->prepare("SELECT status, created_at FROM status WHERE user_id=? ORDER BY id DESC LIMIT 1");
     $stmt2->execute([$user['id']]);
     $status = $stmt2->fetch();
     if ($status) {
+        $time =
         $users[$key]['status']['created_at'] = $status['created_at'];
         $users[$key]['status']['text'] = $status['status'];
     } else {
